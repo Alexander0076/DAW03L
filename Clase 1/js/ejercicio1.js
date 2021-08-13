3c3d2a531000f0bfdb778e4cdc45e11e9fdba586
Alexander0076 <uliseselias007@gmail.com>
2021-08-12 23:36:41 -0600

    js modificado

--- Clase 1/js/ejercicio1.js
+++ Clase 1/js/ejercicio1.js
@@ -9,33 +9,33 @@ function convertir(){
     switch(tipo){
         case "1":
             d=parseFloat(num);
-            e=parseFloat(num);
-            y=parseFloat(num)*110.43;
-            b=parseFloat(num)*45523.20;
+            e=parseFloat(num)*0.852131;
+            y=parseFloat(num)*110.40;
+            b=parseFloat(num)/45523.20;
             break;
         case "2":
-            d=parseFloat(num);
             e=parseFloat(num);
-            y=parseFloat(num);
-            b=parseFloat(num);            ;
+            d=parseFloat(num)/0.852131;
+            y=parseFloat(num)*129.62;
+            b=parseFloat(num)/38461.53;            ;
             break;
         case "3":
-            d=parseFloat(num);
-            e=parseFloat(num);
             y=parseFloat(num);
-            b=parseFloat(num);
+            d=parseFloat(num)/110.40;
+            e=parseFloat(num)/129.62;
+            b=parseFloat(num)/4947646.48;
             break;
         case "4":
-            d=parseFloat(num);
-            e=parseFloat(num);
-            y=parseFloat(num);
             b=parseFloat(num);
+            d=parseFloat(num)*45523.20;
+            e=parseFloat(num)*38461.53;
+            y=parseFloat(num)*4947646.48;
             break;
 
     }
     texto +="Dolares: $"+d+"<br>";
-    texto+="Euros: "+e+"Є<br>";
-    texto+="Yen: "+y+"¥<br>";
-    texto+="Bitcoin: "+b+"BTC<br>";
+    texto+="Euros: Є"+e+"<br>";
+    texto+="Yen: ¥"+y+"<br>";
+    texto+="Bitcoin: ฿"+b+"<br>";
     document.getElementById('datos').innerHTML=texto;
 }
--- Clase 1/js/ejercicio2.js
+++ Clase 1/js/ejercicio2.js
@@ -3,13 +3,31 @@ function Matricular() {
     nombre= document.getElementById('txtnombre').value;
     edad= document.getElementById('txtedad').value;
     alumno = new Array(nombre,edad);
+    suma = 0;
+    media= 0;
+    max=[0];
+    min=[0];
     info.push(alumno);
     tabla="";
     tabla+="<table>";
     tabla+="<tr><td>Nombre</td><td>Edad</td></tr>";
     for(i=0; i<info.length; i++){
         tabla+="<tr><td>"+info[i][0]+"</td><td>"+info[i][1]+"</td></tr>";
+        edades=info[i][1];
+        suma += parseInt(edades);
+        
+        if(max < edades) {
+            max = edades; 
+        }else{
+            min = edades; 
+        }
+        
     }
+
+    media = suma / info.length;
+    tabla+="<tr><td>Edad maxima:</td><td>"+max+"</td></tr>";
+    tabla+="<tr><td>Edad minima:</td><td>"+min+"</td></tr>";
+    tabla+="<tr><td>Promedio de edad:</td><td>"+media+"</td></tr>";
     tabla+="</table>";
     document.getElementById('info').innerHTML=tabla+"<br>Total de alumnos "+info.length;
 }
