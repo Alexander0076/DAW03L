8ffffa964f904700e6f0f035c1620e57821ceba6
Alexander0076 <uliseselias007@gmail.com>
2021-08-15 13:08:52 -0600

    new alert

--- Clase 1/js/ejercicio1.js
+++ Clase 1/js/ejercicio1.js
@@ -1,43 +1,65 @@
 
 function convertir(){
-    
     var num=document.getElementById('txttipo').value;
     tipo=document.getElementById('tipo').value;
     table="";
     var d=0, e=0, y=0, b=0;
+    if (num !== 0 && tipo !== 0){
+        switch(tipo){
+            case "1":
+                d=parseFloat(num);
+                e=parseFloat(num)*0.852131;
+                y=parseFloat(num)*110.40;
+                b=parseFloat(num)/45523.20;
+                break;
+            case "2":
+                e=parseFloat(num);
+                d=parseFloat(num)/0.852131;
+                y=parseFloat(num)*129.62;
+                b=parseFloat(num)/38461.53;            ;
+                break;
+            case "3":
+                y=parseFloat(num);
+                d=parseFloat(num)/110.40;
+                e=parseFloat(num)/129.62;
+                b=parseFloat(num)/4947646.48;
+                break;
+            case "4":
+                b=parseFloat(num);
+                d=parseFloat(num)*45523.20;
+                e=parseFloat(num)*38461.53;
+                y=parseFloat(num)*4947646.48;
+                break;
     
-    switch(tipo){
-        case "1":
-            d=parseFloat(num);
-            e=parseFloat(num)*0.852131;
-            y=parseFloat(num)*110.40;
-            b=parseFloat(num)/45523.20;
-            break;
-        case "2":
-            e=parseFloat(num);
-            d=parseFloat(num)/0.852131;
-            y=parseFloat(num)*129.62;
-            b=parseFloat(num)/38461.53;            ;
-            break;
-        case "3":
-            y=parseFloat(num);
-            d=parseFloat(num)/110.40;
-            e=parseFloat(num)/129.62;
-            b=parseFloat(num)/4947646.48;
-            break;
-        case "4":
-            b=parseFloat(num);
-            d=parseFloat(num)*45523.20;
-            e=parseFloat(num)*38461.53;
-            y=parseFloat(num)*4947646.48;
-            break;
-
+        }
+        table+="<table class='table table-dark table-hover'>"
+        table+="<tr><td>Dolares</td><td>$"+d+"</td></tr>";
+        table+="<tr><td>Euros</td><td>Є"+e+"</td></tr>";
+        table+="<tr><td>Yen</td><td>¥"+y+"</td></tr>";
+        table+="<tr><td>Bitcoin</td><td>฿"+b+"</td></tr>";
+        table+="</table>";
+        document.getElementById('datos').innerHTML=table;
+    }else if (num == 0 && tipo == 0){
+        swal({
+            title: 'Error!',
+            text: 'Por favor llenar los campos',
+            icon: 'error'
+        });
+        document.getElementById('datos').innerHTML=null;
+    }else if(num == 0){
+        swal({
+            title: 'Error!',
+            text: 'Por favor ingrese una cantidad numerica',
+            icon: 'error'
+        });
+        document.getElementById('datos').innerHTML=null;
+    }else if (tipo == '0') {
+        swal({
+            title: 'Error!',
+            text: 'Por favor seleccione una opción',
+            icon: 'error'
+        });
+        document.getElementById('datos').innerHTML=null;
     }
-    table+="<table class='table table-dark table-hover'>"
-    table+="<tr><td>Dolares</td><td>$"+d+"</td></tr>";
-    table+="<tr><td>Euros</td><td>Є"+e+"</td></tr>";
-    table+="<tr><td>Yen</td><td>¥"+y+"</td></tr>";
-    table+="<tr><td>Bitcoin</td><td>฿"+b+"</td></tr>";
-    table+="</table>";
-    document.getElementById('datos').innerHTML=table;
+    
 }
--- Clase 1/js/ejercicio2.js
+++ Clase 1/js/ejercicio2.js
@@ -2,14 +2,15 @@ var info=[];
 function Matricular() {
     nombre= document.getElementById('txtnombre').value;
     edad= document.getElementById('txtedad').value;
-    alumno = new Array(nombre,edad);
     edades=0;
     suma = 0;
     media= 0;
     max=[0];
     min=[0];
-    info.push(alumno);
-    tabla="";
+    if (nombre !== (' ') && edad !== (' ')) {
+        alumno = new Array(nombre,edad);
+        info.push(alumno);
+        tabla="";
     tabla+="<table class='table'>";
     tabla+="<thead><tr><th scope='col'>Nombre</th><th scope='col'>Edad</th></tr></thead>";
     for(i=0; i<info.length; i++){
@@ -29,5 +30,27 @@ function Matricular() {
     tabla+="<thead><tr><th scope='col'>Edad maxima</th><th scope='col'>Edad minima</th></tr></thead>";
     tabla+="<tr><td>"+max+"</td><td>"+min+"</td></tr>";
     tabla+="</table>";
-    document.getElementById('info').innerHTML=tabla+"<br>Promedio de edad: "+media+"<br>Total de alumnos "+info.length;
+    document.getElementById('info').innerHTML=tabla+"<br>Promedio de edad: "+media+"<br>Total de alumnos: "+info.length+"<br><input type='button' class='btn btn-outline-dark' id='btn' value='Borrar lista' onclick='Eliminar()'>";
+    
+    }
+    if (nombre == 0 && edad == 0) {
+        info.splice(0);
+        swal({
+            title: 'Error!',
+            text: 'Por favor llenar los campos',
+            icon: 'error'
+        });
+        document.getElementById('info').innerHTML=null;
+    }else if(nombre == 0 || edad == 0){
+        info.splice(0);
+        swal({
+            title: 'Error!',
+            text: 'Por favor llenar los campos',
+            icon: 'error'
+        });
+        document.getElementById('info').innerHTML=null;
+    }
+
+    
+    
 }
